58b4469269a579d6266fcd22c078889e9ddb8482
Teodor Golisharski <t.golisharski@abv.bg>
2024-10-17 00:27:41 +0300

    First demo added

---
+++ Sandbox Solutions/Reflection/ReflectionDemo1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReflectionDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
