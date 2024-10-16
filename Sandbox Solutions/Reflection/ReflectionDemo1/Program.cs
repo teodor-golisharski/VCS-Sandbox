ce8b7dc78356ec4e6dea7b221304c8fab3a2f81b
Teodor Golisharski <t.golisharski@abv.bg>
2024-10-17 00:31:27 +0300

    class Student added

--- Sandbox Solutions/Reflection/ReflectionDemo1/Program.cs
+++ Sandbox Solutions/Reflection/ReflectionDemo1/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class Program
     {
+        class Student
+        {
+            public string Name { get; set; } = null!;
+            public int Grade { get; set; } 
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
