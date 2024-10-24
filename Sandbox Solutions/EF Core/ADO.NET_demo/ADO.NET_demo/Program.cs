aa561da073d4b170261abeb49e87dc41c099bd5b
Teodor Golisharski <t.golisharski@abv.bg>
2024-10-24 19:35:39 +0300

    Insert method added using query parameterization technique

--- Sandbox Solutions/EF Core/ADO.NET_demo/ADO.NET_demo/Program.cs
+++ Sandbox Solutions/EF Core/ADO.NET_demo/ADO.NET_demo/Program.cs
@@ -4,6 +4,22 @@ namespace ADO.NET_demo
 {
     internal class Program
     {
+        public void Insert(SqlConnection connection)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO Krum " + "(number, first_name, last_name) VALUES " +
+                    "(@number, @first_name, @last_name)", connection);
+
+            int number = int.Parse(Console.ReadLine()!);
+            string first_name = Console.ReadLine()!;
+            string last_name = Console.ReadLine()!;
+
+            cmd.Parameters.AddWithValue("@number", number);
+            cmd.Parameters.AddWithValue("@first_name", first_name);
+            cmd.Parameters.AddWithValue("@last_name", last_name);
+
+            cmd.ExecuteNonQuery();
+        }
+
         static async Task Main(string[] args)
         {
             string connectionString = "Server=.;Database=Demo;Trusted_Connection=True;TrustServerCertificate=True;";
@@ -14,18 +30,20 @@ namespace ADO.NET_demo
             using (connection)
             {
                 SqlCommand command = new SqlCommand("SELECT * FROM Krum;", connection);
+
                 SqlDataReader reader = await command.ExecuteReaderAsync();
 
                 using (reader)
                 {
-                    reader.Read();
-                    string name = (string)reader["first_name"];
-                    string last_name = (string)reader["last_name"];
+                    while (reader.Read())
+                    {
+                        int number = (int)reader["number"];
+                        string name = (string)reader["first_name"];
+                        string last_name = (string)reader["last_name"];
 
-                    Console.WriteLine(name+" "+last_name);
+                        Console.WriteLine(number + " " + name + " " + last_name);
+                    }
                 }
-
-                
             }
         }
     }
