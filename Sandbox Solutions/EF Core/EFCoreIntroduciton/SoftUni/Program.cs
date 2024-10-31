0c15edc53f12ee03c075ea4d37b36be1f75b1d3f
Teodor Golisharski <t.golisharski@abv.bg>
2024-10-31 22:53:00 +0200

    Queries for Employees full information, Employees with Salary over 50000 and Employees from Research and Development created

--- Sandbox Solutions/EF Core/EFCoreIntroduciton/SoftUni/Program.cs
+++ Sandbox Solutions/EF Core/EFCoreIntroduciton/SoftUni/Program.cs
@@ -1,10 +1,88 @@
-﻿namespace SoftUni
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
 {
-    public class Program
+    public class StartUp
     {
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .AsNoTracking()
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    Id = e.EmployeeId,
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    MiddleName = e.MiddleName,
+                    JobTitle = e.JobTitle,
+                    Salary = e.Salary
+                }).ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .AsNoTracking()
+                .OrderBy(e => e.FirstName)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    Salary = e.Salary
+                }).Where(e => e.Salary > 50000)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .AsNoTracking()
+                .Where(ep => ep.Department.Name == "Research and Development")
+                .Select(ep => new
+                {
+                    FirstName = ep.FirstName,
+                    LastName = ep.LastName,
+                    Salary = ep.Salary,
+                })
+                .OrderBy(ep => ep.Salary)
+                .ThenByDescending(ep => ep.FirstName)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from Research and Development - ${employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
         static void Main(string[] args)
         {
-
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
         }
     }
 }
